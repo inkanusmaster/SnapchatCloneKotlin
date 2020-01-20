624f7405966db9abbb158cd03cc386e55354b2e2
inkanusmaster <furymistrz@gmail.com>
2020-01-20 23:15:58 +0100

    Jest już szkielecik.

--- app/src/main/java/mfuryw/snapchatclonekotlin/MainActivity.kt
+++ app/src/main/java/mfuryw/snapchatclonekotlin/MainActivity.kt
@@ -1,12 +1,56 @@
 package mfuryw.snapchatclonekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
+@Suppress("NAME_SHADOWING")
 class MainActivity : AppCompatActivity() {
+
+    var emailEditText: EditText? = null // ? pozwala na nullable
+    var passwordEditText: EditText? = null
+    private lateinit var auth: FirebaseAuth // deklarujemy instancję FirebaseAuth (z dokumentacji firebase ta linijka)
+
+    fun signupLogin(view: View) {
+        // sprawdzamy czy możemy zalogować się
+        auth.signInWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString()).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                logIn()
+                Toast.makeText(this, "Login success.", Toast.LENGTH_SHORT).show()
+            } else {
+                // zakładamy konto
+                auth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString()).addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // dodanie usera do bazy i zalogowanie się
+                        logIn()
+                        Toast.makeText(this, "User created.", Toast.LENGTH_SHORT).show() // zakładanie usera
+                    } else {
+                        Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    fun logIn() {
+        val intent = Intent(this, SnapsActivity::class.java) // nie wiem czemu .java
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance() // inicjalizujemy instancję Firebase (z dokumentacji firebase ta linijka)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        if (auth.currentUser != null) {
+            logIn()
+        }
     }
 }
---
+++ app/src/main/java/mfuryw/snapchatclonekotlin/SnapsActivity.kt
@@ -0,0 +1,41 @@
+package mfuryw.snapchatclonekotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+class SnapsActivity : AppCompatActivity() {
+
+    val auth = FirebaseAuth.getInstance()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // tworzymy menu opcje trzy kropki
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // co będzie jak klikniemy na item z menu
+        if (item?.itemId == R.id.addsnap) {
+            val intent = Intent(this, CreateSnapActivity::class.java)
+            startActivity(intent)
+
+        } else if (item?.itemId == R.id.logout) {
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() { // jak user kliknie strzałkę back
+        super.onBackPressed()
+        auth.signOut()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snaps)
+    }
+}
