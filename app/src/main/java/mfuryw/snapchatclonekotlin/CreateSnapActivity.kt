7d5ccc51cfafbd0bbcca388582daea3e9b0c8a16
inkanusmaster <furymistrz@gmail.com>
2020-01-21 20:04:42 +0100

    Wrzuca obrazy do firebase. Pojebany ten kotlin kurwa. W cały świat wszystko deprecated

--- app/src/main/java/mfuryw/snapchatclonekotlin/CreateSnapActivity.kt
+++ app/src/main/java/mfuryw/snapchatclonekotlin/CreateSnapActivity.kt
@@ -1,12 +1,92 @@
 package mfuryw.snapchatclonekotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import java.io.ByteArrayOutputStream
+import java.util.*
 
+
+@Suppress("DEPRECATION")
 class CreateSnapActivity : AppCompatActivity() {
+
+    var createSnapImageView: ImageView? = null
+    var messageEditText: EditText? = null
+    private val imageName = UUID.randomUUID().toString() + ".jpeg" // losowa nazwa pliku
+
+    private fun getPhoto() { // robimy intent do pobierania foteczek i startujemy startactivityforresult (to służy do przekazania danych z powrotem z intentu drugiego do pierwszego)
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 1)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun chooseImage(view: View) {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            getPhoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val selectedImage = data!!.data
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                createSnapImageView?.setImageBitmap(bitmap)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto()
+            }
+        }
+    }
+
+    fun next (view: View) {
+        createSnapImageView?.isDrawingCacheEnabled = true
+        createSnapImageView?.buildDrawingCache()
+        val bitmap = (createSnapImageView?.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask = FirebaseStorage.getInstance().reference.child("images").child(imageName).putBytes(data)
+        uploadTask.addOnFailureListener {
+            Toast.makeText(this, "Failed to upload image!", Toast.LENGTH_LONG).show()
+
+        }.addOnSuccessListener {
+            Toast.makeText(this, "Image uploaded with success!", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snap)
+
+        createSnapImageView = findViewById(R.id.createSnapImageView)
+        messageEditText = findViewById(R.id.messageEditText)
     }
 }
